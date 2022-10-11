26d91749daaa36c8caadf1d368d53019e2eb59fd
HichamBoutuil <hicham.boutuil@student.ehb.be>
2022-10-11 17:39:39 +0200

    SpaceInvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,98 @@
 "use strict";
+let canvas = document.querySelector("canvas")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+let context = canvas.getContext("2d")
 
-alert("Draw your space invader here");
+
+
+context.fillStyle= "black"
+context.beginPath
+context.Rect(300,300,300,300)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(200,140,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(300,40,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(100,40,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(1,140,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(1,240,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(100,240,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(300,240,100,100)
+context.stroke();
+context.fill();
+
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(400,240,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(400,140,100,100)
+context.stroke();
+context.fill();
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(200,340,100,100)
+context.stroke();
+context.fill();
+
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(200,340,100,100)
+context.stroke();
+context.fill();
+
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(1,450,100,100)
+context.stroke();
+context.fill();
+
+
+context.fillStyle = "green";
+context.beginPath
+context.rect(400,450,100,100)
+context.stroke();
+context.fill();
+
+
+
